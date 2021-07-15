691ebc63dee44469dcbe554feb4f77abc4fd6727
Ana Beatriz Araújo de Sousa <ana.araujo.de.sousa@avanade.com>
2021-07-15 16:19:20 -0300

    Agendamento concluido

--- cypress/integration/agendamento.spec.js
+++ cypress/integration/agendamento.spec.js
@@ -1,9 +1,27 @@
 /// <reference types="cypress"/>
 
+import Agendamento from "../support/Pages/Agendamento";
 import Home from "../support/Pages/Home"
+import Login from "../support/Pages/Login"
 
+beforeEach(() => {
+    cy.visit('/');
+    Home.MarcarConsulta();
+    Login.inserirUsuarioCorreto();
+    Login.inserirSenhaCorreta();
+    Login.clicarLogin();
+    let titulo = 'Make Appointment'
+    Login.verificarLoginSucesso(titulo);
+})
 context('Agendamento', () => {
-    it('Realizar agendamento', () => {
-        
+    it('Realizar agendamento com sucesso', () => {
+        Agendamento.clicarNoCheckbox();
+        Agendamento.clicarNoCalendario();
+        Agendamento.escolharData();
+        let comentario = 'Isso é um comentario';
+        Agendamento.comentar(comentario);
+        Agendamento.marcarConsulta();
+        let agendamentoSucesso = 'Appointment Confirmation';
+        Agendamento.verificarConsultaMarcada(agendamentoSucesso);
     })
 }) 
---
+++ cypress/support/Pages/Agendamento/elements.js
@@ -0,0 +1,9 @@
+export const ELEMENTS = {
+    checkbox : '#chk_hospotal_readmission',
+    calendar : '.input-group-addon',
+    td: '.day',
+    date: '30',
+    comment: '#txt_comment',
+    bookAppointment: '#btn-book-appointment',
+    appointmentConfirmation : 'h2'
+}
